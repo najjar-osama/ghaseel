1f466bce47bff4e238e8605c2b3f862eae4a50fc
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-06-22 21:32:59 +0200

    set styles folder structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "normalize.css";
-import "./App.css";
+import "./styles/styles.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <h1>Ghaseel App!!</h1>;
   }
 }
 
