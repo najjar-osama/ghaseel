94e11ba49c293399350da32518a522c803e22418
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-06-23 02:40:39 +0200

    router is working as expected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
 import createStore from "./store/configureStore";
-import { login } from "./actions/auth";
+import AppRouter from "./routers/AppRouter";
 import "normalize.css";
 import "./styles/styles.css";
 
 const store = createStore();
-console.log(store.getState());
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-setTimeout(() => {
-  store.dispatch(login("123abc"));
-}, 3000);
 
 const App = () => (
   <Provider store={store}>
-    <div className="main">
-      <h1>Ghaseel App!</h1>
-    </div>
+    <AppRouter />
   </Provider>
 );
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { history } from "../routers/AppRouter";
 
 const LoginPage = () => (
   <div>
     <h1>Login page</h1>
-    <button>Login</button>
+    <button
+      onClick={() => {
+        history.push("/home");
+      }}
+    >
+      Login
+    </button>
   </div>
 );
 
