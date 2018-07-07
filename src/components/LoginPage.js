869d0967d1ae57a55d0cebc4151e62c2b056bf8b
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-07-07 16:34:36 +0200

    added brand link

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,15 +2,18 @@ import React from "react";
 import { history } from "../router/AppRouter";
 import { connect } from "react-redux";
 import { login } from "../actions/auth";
+import { Link } from "react-router-dom";
 import logo from "../ghaseel-logo.svg";
 export const LoginPage = props => (
   <div className="app-cover">
     <div className="main-container flex flex--center flex-dir--col text-center opacity-effect">
       <form className="login-form flex flex--center flex-dir--col shadow">
-        <div className="logo-block flex flex--center flex-dir--row">
-          <img className="logo-block__logo" src={logo} />
-          <h1 className="logo-block__title">Ghaseel</h1>
-        </div>
+        <Link to="/" className="brand-link">
+          <div className="logo-block flex flex--center flex-dir--row">
+            <img className="logo-block__logo" src={logo} />
+            <h1 className="logo-block__title">Ghaseel</h1>
+          </div>
+        </Link>
         <input
           type="text"
           className="login-form__input"
