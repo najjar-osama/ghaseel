715ad471879df1b0d4f8e5fe19c23b1369590576
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-07-21 06:51:26 +0200

    corrected a typo

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -5,7 +5,7 @@ export const ProfilePage = props => (
   <div className="main-container flex flex--center flex-dir--col">
     <h1>{`Hi ${props.displayName}!`}</h1>
     <Link to="/home">
-      <h4>My Profile</h4>
+      <h4>Home Page</h4>
     </Link>
   </div>
 );
