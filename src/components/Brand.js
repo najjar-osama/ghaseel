85571b6d165a93f4ee578677b451ffcfd30518c5
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-07-22 02:51:17 +0200

    updated brand logo, added brand logo text, updated nav bar height

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 import logo from "../ghaseel-logo-white.svg";
 export const Brand = () => (
   <div className="brand-logo">
-    <Link to="/home">
+    <Link to="/home" className="brand-logo__link flex flex--center-v">
       <img className="brand-logo__image" src={logo} alt="Ghaseel Logo" />
+      <span className="brand-logo__text">Ghaseel</span>
     </Link>
   </div>
 );
