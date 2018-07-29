34e71e0735b81642737bf2d7e2d537586e1ee757
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-07-29 02:07:43 +0200

    created finilize basic signup form, it's ready to be coupled with an api call / firebase signup

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,47 +1,116 @@
 import React from "react";
+import isEmail from "validator/lib/isEmail";
 
 class SignupForm extends React.Component {
   constructor(props) {
+    super(props);
     this.state = {
       email: "",
       password: "",
       confirmPassword: "",
       hasError: false,
-      errorMessage: ""
+      errorMessage: "",
+      formSubmitted: false
     };
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange(this);
+    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(
+      this
+    );
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
-  handleEmailChange(event){
-    const nextState = { email : event.target.value};
-    this.setState(()=>(nextState));
+  handleEmailChange(event) {
+    const nextState = { email: event.target.value };
+    this.setState(() => nextState);
   }
-  handlePasswordChange(event){
-      const nextState = {password : event.target.value};
-      this.setState(()=>(this.nextState));
+  handlePasswordChange(event) {
+    const nextState = { password: event.target.value };
+    this.setState(() => nextState);
   }
-  handleConfirmPasswordChange(event){
-    const nextState = { confirmPassword : event.target.value};
-    this.setState(()=>(nextState));
+  handleConfirmPasswordChange(event) {
+    const nextState = { confirmPassword: event.target.value };
+    this.setState(() => nextState);
   }
-  handleFormSubmit(event){
-      event.preventDefault();
+  handleFormSubmit(event) {
+    event.preventDefault();
+    if (!isEmail(event.target.email.value)) {
+      const errorEmailNextState = {
+        hasError: true,
+        errorMessage:
+          "invalid email address, please enter a valid email address"
+      };
+      this.setState(() => errorEmailNextState);
+      return;
+    }
+    const password = event.target.password.value.trim() || "";
+    const confirmPassword = event.target.confirmPassword.value.trim() || "";
+    const passwordsMatch = password === confirmPassword;
+    if (!passwordsMatch) {
+      const errorPasswordsNextState = {
+        hasError: true,
+        errorMessage: "passwords don't match, please renter your passwords."
+      };
+      this.setState(() => errorPasswordsNextState);
+      return;
+    }
+
+    const submitNextState = {
+      formSubmitted: true,
+      hasError: false,
+      errorMessage: ""
+    };
+    this.setState(
+      () => submitNextState,
+      () => {
+        // redirect to a protected page 'cause at this point user is authenticated
+        setTimeout(() => {
+          this.setState(() => ({
+            formSubmitted: false,
+            email: "",
+            password: "",
+            confirmPassword: "",
+            hasError: false,
+            errorMessage: ""
+          }));
+        }, 3000);
+      }
+    );
   }
-  render(){
-      return(
-        <form onSubmit={this.handleFormSubmit}>
-            <label htmlFor="email" aria-label="email"></label>
-            <input type="text" name="email" value={this.state.email} onChange={this.handleEmailChange}/>
-            <label htmlFor="password" aria-label="password"></label>
-            <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
-            <label htmlFor="password" aria-label="confirm-password"></label>
-            <input type="password" name="confirm-password" value={this.state.confirmPassword} onChange={this.handleConfirmPasswordChange}/>
-        </form>
-      );
+  render() {
+    const loader = <div>Submitting..</div>;
+    const form = (
+      <form onSubmit={this.handleFormSubmit}>
+        <label htmlFor="email" aria-label="email" />
+        <input
+          type="text"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <label htmlFor="password" aria-label="password" />
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <label htmlFor="password" aria-label="confirm password" />
+        <input
+          type="password"
+          name="confirmPassword"
+          value={this.state.confirmPassword}
+          onChange={this.handleConfirmPasswordChange}
+        />
+        {this.state.hasError && (
+          <div>
+            <p>{this.state.errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Signup</button>
+      </form>
+    );
+    return this.state.formSubmitted ? loader : form;
   }
 }
-
 
 export default SignupForm;
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import SignupForm from "./SignupForm";
 
 export const SignupPage = () => (
   <div className="main-container flex flex--center flex-dir--col">
     <h1>Welcome to Signup Page!</h1>
+    <SignupForm />
   </div>
 );
 
