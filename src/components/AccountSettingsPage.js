f691165c1bdc25f2568f92a3947929df0913f114
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-07-30 05:11:09 +0200

    some basic styling for readability

--- src/components/AccountSettingsPage.js
+++ src/components/AccountSettingsPage.js
@@ -2,9 +2,11 @@ import React from "react";
 import InitAccountSettingsForm from "./InitAccountSettingsForm";
 
 export const AccountSettingsPage = () => (
-  <div>
+  <div className="main-container flex flex--center flex-dir--col">
     <h1>Account Setting Page!</h1>
-    <InitAccountSettingsForm />
+    <div className="form-wrapper">
+      <InitAccountSettingsForm />
+    </div>
   </div>
 );
 
