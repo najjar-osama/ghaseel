94a277d4ffb0b3d62b15276237a62f801a08e0ab
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-07-30 05:58:35 +0200

    added link to home page in order to prevent locking on a page

--- src/components/AccountSettingsPage.js
+++ src/components/AccountSettingsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import InitAccountSettingsForm from "./InitAccountSettingsForm";
+import { Link } from "react-router-dom";
 
 export const AccountSettingsPage = () => (
   <div className="main-container flex flex--center flex-dir--col">
     <h1>Account Setting Page!</h1>
     <div className="form-wrapper">
       <InitAccountSettingsForm />
+      <Link to="/home">Home</Link>
     </div>
   </div>
 );
